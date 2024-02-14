c8e55f24877df70949eb01fa6d54fc13deb842d2
abdurrozaqf <rozaqa27@gmail.com>
2024-02-14 15:10:46 +0700

    feat: update project detail pages

--- src/modules/projects/components/ProjectDetail.tsx
+++ src/modules/projects/components/ProjectDetail.tsx
@@ -1,13 +1,15 @@
-import { STACKS } from "@/common/constant/stacks";
 import Image from "next/image";
-
-import { DetailRepositories } from "@/common/types/response";
-import SectionHeading from "@/components/elements/SectionHeading";
-import { ScreenShare } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+
+import { ScreenShare } from "lucide-react";
 import { SiGithub } from "react-icons/si";
+
+import { Separator } from "@/components/ui/separator";
+
 import { MENU_REPOSITORES } from "@/common/constant/repositories";
+import { DetailRepositories } from "@/common/types/response";
+import { STACKS } from "@/common/constant/stacks";
 
 interface ProjectDetail {
   data: DetailRepositories;
@@ -35,7 +37,7 @@ export default function ProjectDetail({ data }: ProjectDetail) {
             href={data.html_url}
             className="flex items-center gap-x-2"
           >
-            <SiGithub />
+            <SiGithub size={20} />
             <span>Source Code</span>
           </Link>
           <Link
@@ -44,7 +46,7 @@ export default function ProjectDetail({ data }: ProjectDetail) {
             target="_blank"
             className="flex items-center gap-x-2"
           >
-            <ScreenShare />
+            <ScreenShare size={20} />
             <span>Live Demo</span>
           </Link>
         </div>
@@ -59,6 +61,17 @@ export default function ProjectDetail({ data }: ProjectDetail) {
           sizes="100vw"
           className="w-full h-[404.28px] object-cover object-top rounded"
         />
+      </div>
+      <Separator className="my-2" />
+      <div className="space-y-2">
+        <p className="text-xl font-bold">Stack Used</p>
+        <ul className="ml-5 list-disc space-y-2">
+          {detail?.tech_stacks.map((item) => (
+            <li key={item}>
+              <p>{item}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   );
