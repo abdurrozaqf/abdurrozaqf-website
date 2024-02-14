7ea53811ad561269a7005635c97b0054c172d2f2
abdurrozaqf <rozaqa27@gmail.com>
2024-02-14 14:15:08 +0700

    feat: update motion

--- src/modules/about/components/Summary.tsx
+++ src/modules/about/components/Summary.tsx
@@ -1,37 +1,27 @@
-import { User2 } from "lucide-react";
+"use client";
+
+import { motion } from "framer-motion";
 import React from "react";
 
-import SectionSubHeading from "@/components/elements/SectionSubHeading";
-import SectionHeading from "@/components/elements/SectionHeading";
 import { summaryMock } from "@/common/mocks/summary";
 
 interface SummaryProps {}
 
 export default function Summary({}: SummaryProps) {
+  const paragraphs = Object.values(summaryMock);
   return (
-    <section className="space-y-6">
-      <div className="space-y-2">
-        <SectionHeading
-          title="Profile Summary"
-          icon={<User2 className="mr-1" />}
-        />
-        <SectionSubHeading>
-          <p className="dark:text-neutral-400">About me</p>
-        </SectionSubHeading>
-      </div>
-      <div className="space-y-4 leading-[1.8] text-neutral-800 dark:text-neutral-300 md:leading-loose text-justify">
-        <p data-aos="fade-left" data-aos-delay="100">
-          {summaryMock.paragraphOne}
-        </p>
-        <p data-aos="fade-left" data-aos-delay="200">
-          {summaryMock.paragraphTwo}
-        </p>
-        <p data-aos="fade-left" data-aos-delay="300">
-          {summaryMock.paragraphThree}
-        </p>
-        <p data-aos="fade-left" data-aos-delay="400">
-          {summaryMock.paragraphFour}
-        </p>
+    <section>
+      <div className="space-y-4 leading-[1.4] text-neutral-800 dark:text-neutral-300 md:leading-[1.6] text-justify">
+        {paragraphs.map((item: string, index: number) => (
+          <motion.div
+            key={index}
+            initial={{ x: 12, opacity: 0, scale: 0.9 }}
+            animate={{ x: 0, opacity: 1, scale: 1 }}
+            transition={{ duration: 0.3, delay: index * 0.2 }}
+          >
+            <p>{item}</p>
+          </motion.div>
+        ))}
       </div>
     </section>
   );
